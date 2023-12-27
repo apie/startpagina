e5f2fa0615769720339ca7f3ef0daae55de987ef
Denick <1049485+apie@users.noreply.github.com>
2023-12-27 16:54:58 +0100

    add systemd + nginx files

--- start.bash
+++ start.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+PORT=${1:-5000}
 if [ ! -f 'app.py' ]; then
   echo 'Only run from within the dir'
   exit 1
@@ -11,5 +12,5 @@ source venv/bin/activate
 pip3 install pip==23.3.1 pip-tools==7.3.0
 pip-sync setup/requirements.txt
 
-hypercorn --bind 0.0.0.0:5000 app
+hypercorn --bind 0.0.0.0:$PORT app
 
