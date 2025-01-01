5d95ce3144ca232a527545bc3adf19a5318351df
Denick <1049485+apie@users.noreply.github.com>
2025-01-01 22:30:15 +0100

    black

--- app.py
+++ app.py
@@ -11,13 +11,19 @@ app = Quart(__name__)
 
 async def tvmaze():
     retval = []
-    for show in sorted(get_followed_shows(), key=lambda s: s['name']):
-        if acquired_eps := get_acquired_eps(show['show_id']):
-            retval.append({'name': show["name"], 'status': show["status"], 'num_acquired': len(acquired_eps)})
+    for show in sorted(get_followed_shows(), key=lambda s: s["name"]):
+        if acquired_eps := get_acquired_eps(show["show_id"]):
+            retval.append(
+                {
+                    "name": show["name"],
+                    "status": show["status"],
+                    "num_acquired": len(acquired_eps),
+                }
+            )
     return retval
 
 
-@app.route('/')
+@app.route("/")
 async def index():
     shows = await tvmaze()
     aocs = await get_all_open_days_for_user(AOC_NAME)
