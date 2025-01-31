64c0b7742eb7a4f6d2bcb0d63d858095791d39c4
Denick <1049485+apie@users.noreply.github.com>
2025-01-31 15:28:32 +0100

    bump versions

--- start.bash
+++ start.bash
@@ -9,7 +9,7 @@ if [ ! -d "venv" ]; then
   virtualenv --python=python3.12 venv
 fi
 source venv/bin/activate
-pip3 install pip==23.3.1 pip-tools==7.3.0
+pip3 install pip==25.0 pip-tools==7.4.1
 pip-sync setup/requirements.txt
 
 hypercorn --bind 0.0.0.0:$PORT app
